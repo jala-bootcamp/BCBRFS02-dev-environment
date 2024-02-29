a378a21556e0123b4c1c0e214c429b2277213e9c
Sergio Rodriguez <sergio@ffactory.io>
2024-02-29 17:42:07 -0400

    Refactor: Updated several libs and configurations given that versions have changed.

--- app/src/main/groovy/io/ffactory/pokemondemo/entity/Trainer.groovy
+++ app/src/main/groovy/io/ffactory/pokemondemo/entity/Trainer.groovy
@@ -1,12 +1,12 @@
 package io.ffactory.pokemondemo.entity
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.validation.constraints.NotNull
-import javax.persistence.Id
-import javax.persistence.Table
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.validation.constraints.NotNull
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "trainers")
